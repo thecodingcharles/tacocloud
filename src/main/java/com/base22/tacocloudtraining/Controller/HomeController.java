8efe146a047416f172fd55f02a48f3f1a724e56c
thecodingcharles <carlos.perez@corp.base22.com>
2020-02-04 20:49:15 -0600

    created an initial project structure using Spring Initializr. wrote a controller class to handle the homepage request. defined a view template to render the homepage.

---
+++ src/main/java/com/base22/tacocloudtraining/Controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.base22.tacocloudtraining.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping
+    public String home(){
+        return "home";
+    }
+}
---
+++ src/main/java/com/base22/tacocloudtraining/TacocloudTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.base22.tacocloudtraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacocloudTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacocloudTrainingApplication.class, args);
+	}
+
+}
